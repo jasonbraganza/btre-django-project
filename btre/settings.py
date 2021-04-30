03a279c9801d2e35de8978aa28ae8ccb351973ea
Mario Jason Braganza <jason@janusworx.com>
2021-04-30 12:02:19 +0530

    Updated settings.py to work with my .env file
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- btre/settings.py
+++ btre/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [host.strip() for host in os.getenv("ALLOWED_HOSTS").split(",")]
 
 
 # Application definition
